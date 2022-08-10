1626ec3df685a721d41576ce1f7d3523e1d77266
yuri-note <yuri-note@LAPTOP-NI438OED>
2022-08-10 16:58:51 +0900

    문제품

--- 04_ java_string/src/step4_01/string/StringEx18_정답.java
+++ 04_ java_string/src/step4_01/string/StringEx18_정답.java
@@ -1,5 +1,6 @@
 package step4_01.string;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /*
@@ -22,18 +23,58 @@ public class StringEx18_정답 {
 		String meaning = "공연";
 		int size = word.length();
 		int[] check = new int[size];  // 힌트 체크를 통해서 확인할수있다.
+		Random ran = new Random();
+		int letter;
+		int cnt = 0;
 		
 		while (true) {
 			
+			for (int i = 0; i < size; i++) {
+				if(check[i] == 1) {
+					cnt ++;
+				}
+			}
+			if (cnt == size) {
+				System.out.println("정답 : " + word);
+				break;
+			}
+			
 			System.out.println("뜻 : " + meaning);
 			System.out.print("문제 : ");
-			for (int i =0; i< size; i++) {
+			for (int i = 0; i < size; i++) {
+				if (check[i] == 0) System.out.print("*");
+				else if (check[i] == 1) System.out.print(word.charAt(i));
+			}
+			/*for (int i =0; i< size; i++) {
 				System.out.print("*");
 				
-			}
+			}*/
 			System.out.println();
 			System.out.println("영어단어를 입력하세요 >>> ");
 			String me = scan.next();
+			
+		
+			
+			if (word.equals(me)) {
+				System.out.println("정답 : " + word);
+				System.out.println("점수 : " + score);
+				break;
+			}
+			else {
+				while (true) {
+					letter = ran.nextInt(size);
+					if (check[letter] != 1) {
+						check[letter] = 1;
+						for (int i = 0; i < size; i++) {
+							if (word.charAt(letter) == word.charAt(i)) {
+								check[i] = 1;
+								}
+						}
+						score -= 5;
+						break;
+					} 
+				}
+			}
 			
 		}
 
